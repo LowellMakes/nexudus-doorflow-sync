bf8a382098a99f67986ea8db6aebc1613a0c2db6
Ben Brown <brown.b774@gmail.com>
2024-03-02 15:03:15 -0500

    added doorflow updates

--- nexudus_polling.py
+++ nexudus_polling.py
@@ -7,9 +7,11 @@ import logging
 import pandas as pd
 import sqlite3
 
+
 def dict_factory(cursor, row):
     fields = [column[0] for column in cursor.description]
     return {key: value for key, value in zip(fields, row)}
+
 
 team_map = pd.read_csv('team_group_map.csv', dtype='str')
 doorflow_auth = requests.auth.HTTPBasicAuth(
@@ -23,22 +25,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 update_frequency = 5  # in minutes
 
+
 def format_datetime(input):
 
     output = int(pd.to_datetime(input).timestamp())
 
     return output
 
+
 def get_team_updates():
     teams = team_map['nexudus_id'].to_list()
     ids_to_update = []
     for team in teams:
-        if pd.isna(team):
+        if team == 'None':
             continue
         url = f"https://spaces.nexudus.com/api/spaces/teams/{team}"
         r = requests.get(url, auth=nexudus_auth)
         nex_members = r.json()['TeamMembers']
-        cur = con.execute(f"SELECT id from nexudus_members WHERE TeamIds like '%{team}%'")
+        cur = con.execute(
+            f"SELECT id from nexudus_members WHERE TeamIds like '%{team}%'")
         local_members = [int(x['Id']) for x in cur.fetchall()]
         diff = set(nex_members).symmetric_difference(set(local_members))
         if len(diff):
@@ -60,6 +65,7 @@ def get_team_updates():
 
     return all_updates
 
+
 def get_full_nexudus_database():
 
     logging.info('Getting complete record of Nexudus Database')
@@ -77,12 +83,15 @@ def get_full_nexudus_database():
     data = pd.DataFrame(r['Records'])
     data = data.drop(columns=['CustomFields'])
     data = data.astype(str)
-    data['UpdatedOn'] = pd.to_datetime(data['UpdatedOn'])
+    data['UpdatedOn'] = [format_datetime(x) for x in data['UpdatedOn']]
+    data['NeedUpdate'] = False
     data.to_sql(name='nexudus_members', con=con, if_exists='replace')
+
 
 def get_user_updates():
 
-    start_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=update_frequency+1)
+    start_time = datetime.datetime.now(
+        datetime.timezone.utc) - datetime.timedelta(minutes=update_frequency+1)
     logging.info(f'Searching for updates since {start_time}')
 
     start_string = urllib.parse.quote_plus(
@@ -95,14 +104,16 @@ def get_user_updates():
     r = requests.get(url, auth=nexudus_auth)
     if r.status_code == 200:
         for user in r.json()['Records']:
-            logging.info(f"From Nexudus Updates - Adding User: {user['FullName']}")
+            logging.info(
+                f"From Nexudus Updates - Adding User: {user['FullName']}")
             all_ids.append(user['Id'])
 
     url = "https://spaces.nexudus.com/api/billing/coworkerinvoices?CoworkerInvoice_Paid=false&CoworkerInvoice_Void=false"
     r = requests.get(url, auth=nexudus_auth)
     if r.status_code == 200:
         for user in r.json()['Records']:
-            logging.info(f"From Due Invoices - Adding User: {user['CoworkerFullName']}")
+            logging.info(
+                f"From Due Invoices - Adding User: {user['CoworkerFullName']}")
             all_ids.append(user['CoworkerId'])
 
     all_ids = list(set(all_ids))
@@ -118,24 +129,140 @@ def get_user_updates():
 
     return all_updates
 
+
 def update_users(updates):
 
     for update in updates:
         logging.info(f"    Updating user: {update['FullName']}")
         logging.debug(f"    User ID: {update['Id']}")
-        cur = con.execute(f"SELECT * from nexudus_members WHERE id={update['Id']}")
+        cur = con.execute(
+            f"SELECT * from nexudus_members WHERE id={update['Id']}")
         local = cur.fetchone()
         update_flag = False
         for key in local.keys():
-            if key in ['index','UpdatedOn']:
+            if key in ['index', 'UpdatedOn', 'NeedUpdate']:
                 continue
             if str(local[key]) != str(update[key]):
                 update_flag = True
-                con.execute(f"UPDATE nexudus_members SET '{key}'='{update[key]}' WHERE id={update['Id']}")
-                logging.debug(f"    Updating {key} from {local[key]} to {update[key]}")
+                con.execute(
+                    f"UPDATE nexudus_members SET '{key}'='{update[key]}' WHERE id={update['Id']}")
+                logging.debug(
+                    f"    Updating {key} from {local[key]} to {update[key]}")
+                con.execute(
+                    f"UPDATE nexudus_members SET 'NeedUpdate'= True WHERE id={update['Id']}")
         if update_flag:
-            con.execute(f"UPDATE nexudus_members SET 'UpdatedOn'='{format_datetime(datetime.datetime.now(datetime.timezone.utc))}' WHERE id={update['Id']}")
+            con.execute(
+                f"UPDATE nexudus_members SET 'UpdatedOn'='{format_datetime(datetime.datetime.now(datetime.timezone.utc))}' WHERE id={update['Id']}")
         con.commit()
+
+
+def create_groups(team_string):
+
+    group_list = []
+    group_list.append('4482')  # Group ID for Basic Membership
+
+    if team_string == "None":
+        return group_list
+
+    teams = team_string.split(',')
+    for team in teams:
+        doorflow_team = team_map[team_map.nexudus_id == str(team)]
+        if doorflow_team.empty:
+            continue
+        else:
+            group_list.append(doorflow_team.iloc[0].doorflow_id)
+
+    return group_list
+
+
+def get_enable_status(user):
+
+    if user['CoworkerContractIds'] == 'None':
+        return False
+
+    url = f"https://spaces.nexudus.com/api/billing/coworkerinvoices?CoworkerInvoice_Coworker={user['Id']}&CoworkerInvoice_Paid=false&CoworkerInvoice_Void=false"
+    r = requests.get(url, auth=nexudus_auth)
+    r.json()['Records']
+
+    if len(r.json()['Records']):
+        return False
+
+    return True
+
+
+def push_doorflow_changes():
+
+    logging.info("Checking for Doorflow Updates")
+
+    cur = con.execute(
+        f"SELECT * from nexudus_members WHERE NeedUpdate=True")
+    local = cur.fetchall()
+
+    need_sync = False
+
+    if not local:
+        logging.info("No Doorflow updates needed")
+
+    for update_user in local:
+        logging.debug(
+            f"Updating: {update_user['GuessedFirstName']} {update_user['GuessedLastName']}")
+        logging.debug(f"ID: {update_user['Id']}")
+        payload = {
+            "first_name": update_user['GuessedFirstName'],
+            "last_name": update_user['GuessedLastName'],
+            "group_ids": create_groups(update_user['TeamIds']),
+            "enable": get_enable_status(update_user),
+            "email": update_user['Email'],
+            "system_id": 'NX'+str(update_user['Id']),
+            "notes": "Updates by syncTools V3",
+        }
+
+        if update_user['AccessCardId'] not in ['None', '']:
+            payload["credentials_number"] = str(
+                int(update_user['AccessCardId']))
+
+        if update_user['AccessPincode'] not in ['None', '']:
+            payload["pin"] = str(int(update_user['AccessPincode']))
+
+        if update_user['KeyFobNumber'] not in ['None', '']:
+            payload["key_fob_number"] = str(int(update_user['KeyFobNumber']))
+
+        logging.debug(payload)
+
+        url = 'https://api.doorflow.com/api/2/people'
+        r = requests.post(url, auth=doorflow_auth,
+                          json=payload)
+        if r.status_code == 422:
+            logging.debug("Cannot create new user")
+            email = urllib.parse.quote_plus(update_user['Email'], safe='@')
+            url = f"https://admin.doorflow.com/api/2/people?email={email}"
+            doorflow_user = requests.get(url, auth=doorflow_auth)
+            doorflow_id = doorflow_user.json()[0]['id']
+            url = 'https://api.doorflow.com/api/2/person/{}'.format(
+                doorflow_id)
+            r = requests.put(url, auth=doorflow_auth,
+                             json=payload)
+            if r.status_code == 201:
+                logging.debug("User Updated")
+            else:
+                logging.debug("Something went wrong with the doorflow update")
+        else:
+            logging.debug("New User Created")
+
+        con.execute(
+            f"UPDATE nexudus_members SET 'NeedUpdate'= 'False' WHERE id={update_user['Id']}")
+        con.commit()
+        need_sync = True
+
+    return need_sync
+
+
+def push_doorflow_sync():
+
+    logging.info('Pushing Doorflow Sync')
+    url = 'https://api.doorflow.com/api/2/sync'
+    requests.post(url, auth=doorflow_auth)
+
 
 def main():
 
@@ -151,17 +278,17 @@ def main():
         if now < next_run:
             continue
 
-        # try:
         updates = get_user_updates()
         updates += get_team_updates()
         if updates:
             update_users(updates)
-        # except:
-        #     time.sleep(1)
-        #     continue
+            need_sync = push_doorflow_changes()
+            if need_sync:
+                push_doorflow_sync()
 
         next_run = now + datetime.timedelta(minutes=update_frequency)
 
+
 if __name__ == "__main__":
-    # get_full_nexudus_database()
+
     main()
